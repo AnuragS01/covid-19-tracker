723a749ebecb7a11dea2c4363fc131d4a446c581
AnuragS01 <anuragsatyamsingh@gmail.com>
2021-04-03 09:02:06 +0530

    adding project to github

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+import { fetchData } from "./api/index";
+import covidImage from "./images/covid-19-image.png";
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className="container">
+        <img className=".image" src={covidImage} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
